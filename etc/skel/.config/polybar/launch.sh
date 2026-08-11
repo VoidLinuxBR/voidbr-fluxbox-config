ee41b0be011107a9b9bc33a7979a8e8dfbbc45c2
vcatafesta <vcatafesta@gmail.com>
2026-08-10 22:37:14 -0400

    seg 10 ago 2026 22:37:14 -04 Vilmar Catafesta (vcatafesta@gmail.com)

--- etc/skel/.config/polybar/launch.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# Mata instâncias antigas do polybar e sobe de novo com o config Tokyo Night
-
-killall -q polybar
-
-while pgrep -u "$UID" -x polybar >/dev/null; do sleep 1; done
-
-polybar main -c "$HOME/.config/polybar/config.ini" &
--- etc/skel/.config/polybar/scripts/xbps-updates.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# Conta quantos pacotes o xbps tem para atualizar
-count=$(xbps-install -Sun 2>/dev/null | wc -l)
-
-if [ "$count" -gt 0 ]; then
-  echo "$count atualizações"
-else
-  echo "em dia"
-fi
